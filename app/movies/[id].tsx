68fec5a225d72db0963bc4f96d490e5d21598923
AlanRik <alanrik2004@gmail.com>
2025-06-20 17:48:00 -0300

    se empieza a desarrollar el trabajo práctico número 4

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 const MovieDetails = () => {
   return (
     <View className="justify-center items-center flex-1">
-      <Text className='font-bold text-center'>Movie Details: en proceso </Text>
+      <Text className='font-bold text-center'>Movie Details: actualizando </Text>
     </View>
   )
 }
