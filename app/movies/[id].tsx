9124632dd611ed5cae45868d4e7e432629c5742a
Alan <ariquelme312@gmail.com>
2025-06-21 22:25:19 -0300

    Se agregaron estilos e información de los detalles de las películas

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,14 +1,67 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
-const MovieDetails = () => {
-  return (
-    <View className="justify-center items-center flex-1">
-      <Text className='font-bold text-center'>Movie Details: actualizando </Text>
-    </View>
-  )
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
 }
 
-export default MovieDetails
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className="flex-col items-start justify-center mt-5">
+    <Text className="text-violet-300 font-normal text-sm">{label}</Text>
+    <Text className="text-violet-200 font-bold text-sm">{value || "N/A"}</Text>
+  </View>
+);
 
-const styles = StyleSheet.create({})
+const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
+  return (
+    <View className="bg-primary flex-1 ">
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+      >
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+        <View className="flex-col item-star justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+          <View className="flex-row items-center gap-x-1 mt-2">
+            <Text className="text-cyan-400 text-sm">
+              {movie?.release_date?.split("-")[0]}
+            </Text>
+            <Text className="text-cyan-400 text-sm">{movie?.runtime}m</Text>
+          </View>
+          <View className="flex-row item-center bg-slate-900 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className="size-4" />
+            <Text className="text-white text-sm font-bold">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className="text-violet-200 text-sm">
+              ({movie?.vote_count}votes)
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MovieDetails;
+
+const styles = StyleSheet.create({});
